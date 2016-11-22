fa5280a3283f770b3bcf4790b3584a263a440956
Matthew O'Connell <mxoconnell@gmail.com>
2016-11-21 22:37:26 -0800

    init commit

---
+++ Assets/YNTD_FlickerOnOff.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Assertions;
+
+public class YNTD_FlickerOnOff : MonoBehaviour {
+    /// <summary>
+    /// Flickers an item's enabled attribute from enabled to un-enabled, on and off again, within the hierarchy
+    /// </summary>
+    [SerializeField] GameObject itemToFlicker1;
+    [SerializeField] GameObject itemToFlicker2;
+    int numFlicks;
+    int numFlicksNeeded;// Number of flicks until we turn the light on/off
+    bool isFlickering;
+    bool isOn; // If flickering then this is not observed
+    static int NUM_FLICKS_UNTIL_OFF = 100;
+    static int NUM_FLICKS_UNTIL_ON = 2500;
+
+    // Use this for initialization
+    void Start () {
+        Assert.IsNotNull(itemToFlicker1);
+        Assert.IsNotNull(itemToFlicker2);
+        numFlicks = 0;
+        numFlicksNeeded = 0;
+        isFlickering = true;
+        isOn = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        InvokeRepeating("Flicker", 0.0f, 0.9f); // Starting [2nd parameter] seconds, repeating every [3rd parameter] seconds
+    }
+
+    void Flicker()
+    {
+        numFlicks++;
+        if(isFlickering){
+            itemToFlicker1.SetActive(!itemToFlicker1.activeInHierarchy);
+            itemToFlicker2.SetActive(!itemToFlicker2.activeInHierarchy);
+        }
+        if(numFlicks > numFlicksNeeded+Random.Range(100, 500)){
+            // if it was flickering it will now be on or off for a while
+            if(isFlickering)
+            {
+                isOn = (Random.value >= 0.5);
+                itemToFlicker1.SetActive(isOn);
+                itemToFlicker2.SetActive(isOn);
+                numFlicksNeeded = isOn ? NUM_FLICKS_UNTIL_OFF : NUM_FLICKS_UNTIL_ON;
+            }
+            isFlickering = !isFlickering;
+            numFlicks = 0;
+        }
+    }
+
+}
---
+++ Assets/YNTD_Underwater.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Assertions;
+
+public class YNTD_Underwater : MonoBehaviour {
+    /// <summary>
+    /// Okay so this level's triggers are all based on how high above the ground the player is. We trigger off they player's Y.    /// 
+    /// </summary>
+	// Use this for initialization
+    public bool isUnderWater;
+    UnityStandardAssets.ImageEffects.BlurOptimized blur;
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject camera;
+    void Start () {
+        Assert.IsNotNull(player);
+        Assert.IsNotNull(camera);
+        blur = camera.GetComponent<UnityStandardAssets.ImageEffects.BlurOptimized>();
+        Assert.IsNotNull(blur);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        isUnderWater = (player.transform.position.y <= 1.1);
+        blur.enabled = isUnderWater;
+	}
+}
