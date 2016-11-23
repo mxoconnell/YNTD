cbd29eb2e7c64ab90453d03ba2180ae9b82b9c85
Matthew O'Connell <mxoconnell@gmail.com>
2016-11-23 02:59:20 -0800

    Bottle almost floating in water

--- Assets/YNTD_Floating.cs
+++ Assets/YNTD_Floating.cs
@@ -2,20 +2,21 @@
 using System.Collections;
 
 public class YNTD_Floating : MonoBehaviour {
-    public float maxY = 0;
-    public float minY = 0;
+    public double maxY = 0;
+    public double minY = 0;
+    public double mydY = .0005f;
     public bool goingUp = true;
 	// Use this for initialization
 	void Start () {
-	
-	}
+        transform.position = new Vector3(transform.position.x, (float)minY, transform.position.z);
+    }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
         //if(Random.Range(0, 10) > 5)
         //    return;
-        float dY = goingUp ? .0005f : -.0005f;
-        transform.Translate(Vector3.up * dY, Space.World);
+        double dY = goingUp ? mydY : -mydY;
+        transform.Translate(Vector3.up * (float)dY, Space.World);
         if(transform.position.y > maxY || transform.position.y < minY)
             goingUp = !goingUp;
 
