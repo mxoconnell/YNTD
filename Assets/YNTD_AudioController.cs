53c88847eb414f13245c5c56d2f64df468337f22
Matthew O'Connell <mxoconnell@gmail.com>
2016-11-27 23:57:49 -0800

    Added motion blur and light on drinking

--- Assets/YNTD_AudioController.cs
+++ Assets/YNTD_AudioController.cs
@@ -5,6 +5,7 @@ using UnityEngine.Assertions;
 public class YNTD_AudioController : MonoBehaviour {
     /// <summary>
     /// This is the sound machine!
+    /// I have two source's for sound effects in case I want to play two sound effects concurrently.
     /// </summary>
     /// 
     public enum Sounds { SE_Gulp };
@@ -12,7 +13,9 @@ public class YNTD_AudioController : MonoBehaviour {
     [SerializeField] private AudioSource srcOmnipresentBG;
     [SerializeField] private AudioSource srcLocationBasedBG;
     [SerializeField] private AudioSource srcSoundEffects;
+    [SerializeField] private AudioSource srcSoundEffects2;
     [SerializeField] private AudioClip SE_Gulp;
+    [SerializeField] private AudioClip SE_Sigh;
     [SerializeField] private AudioClip BG_UnderWater;
 
 
@@ -21,6 +24,7 @@ public class YNTD_AudioController : MonoBehaviour {
         Assert.IsNotNull(srcOmnipresentBG);
         Assert.IsNotNull(srcLocationBasedBG);
         Assert.IsNotNull(srcSoundEffects);
+        Assert.IsNotNull(srcSoundEffects2);
 
         Assert.IsNotNull(SE_Gulp);
         Assert.IsNotNull(BG_UnderWater);
@@ -32,7 +36,11 @@ public class YNTD_AudioController : MonoBehaviour {
         {
             srcSoundEffects.clip = SE_Gulp;
             srcSoundEffects.Play();
+
+            srcSoundEffects2.clip = SE_Sigh;
+            srcSoundEffects2.PlayDelayed(.5f);
         }
 
     }
+
 }
--- Assets/YNTD_Controller.cs
+++ Assets/YNTD_Controller.cs
@@ -14,6 +14,27 @@ public class YNTD_Controller : MonoBehaviour {
     [SerializeField] private YNTD_textController txtCredit;
     [SerializeField] private YNTD_textController txtPrompt;
 
+    // Pool Dyanic elements (stuff we flicker on Trigger1)
+    [SerializeField] private ReflectionProbe poolProbe;
+    [SerializeField] private Light poolLight;
+
+    // Blur variables (due to drinking)
+    // blurActivated ? use MAXvelocity : use MINvelocity
+    // We are increasing the MIN as we progress through game
+    UnityStandardAssets.ImageEffects.CameraMotionBlur blurScript;
+    static int BLUR_MAX_VELOCITY = 1000000;
+    int BLUR_MIN_VELOCITY = 1;
+    int BLUR_TARGET_VELOCITY = 1;
+    int dBlur = 10000;
+    int curBlur;
+
+
+    // Screen Shake
+    Vector3 originPosition ;
+    Quaternion originRotation;
+    float shake_decay;
+    float shake_intensity;
+     
     /*
      *  0 = before they begin game (main menu
      *  1 = After they begin the game 
@@ -26,15 +47,47 @@ public class YNTD_Controller : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
+        Assert.IsNotNull(poolProbe);
+        Assert.IsNotNull(poolLight);
+
         Assert.IsNotNull(FPSController);
         Assert.IsNotNull(txtTitle);
         Assert.IsNotNull(txtCredit);
         Assert.IsNotNull(txtPrompt);
-
+        blurScript = FPSController.GetComponentInChildren<UnityStandardAssets.ImageEffects.CameraMotionBlur>();
+        Assert.IsNotNull(blurScript);
+        curBlur = BLUR_TARGET_VELOCITY;
     }
 	
 	// Update is called once per frame
 	void Update () {
+
+        if(shake_intensity > 0)
+        {
+            transform.position = originPosition + UnityEngine.Random.insideUnitSphere * shake_intensity;
+            transform.rotation = new Quaternion(
+                            originRotation.x + UnityEngine.Random.Range(-shake_intensity, shake_intensity) * .2f,
+                            originRotation.y + UnityEngine.Random.Range(-shake_intensity, shake_intensity) * .2f,
+                            originRotation.z + UnityEngine.Random.Range(-shake_intensity, shake_intensity) * .2f,
+                            originRotation.w + UnityEngine.Random.Range(-shake_intensity, shake_intensity) * .2f);
+            shake_intensity -= shake_decay;
+        }
+
+
+
+
+
+        Debug.Log("curBlur:" + curBlur + "   |     BLUR_TARGET_VELOCITY:" + BLUR_TARGET_VELOCITY);
+        // Determine which way to inlfuence the blur (due to alcohol)
+        if(curBlur < BLUR_TARGET_VELOCITY-dBlur)
+            curBlur += dBlur;
+        else if(curBlur > BLUR_TARGET_VELOCITY+dBlur)
+            curBlur -= dBlur;
+        // For subtle changes
+        if(Mathf.Abs(curBlur - BLUR_TARGET_VELOCITY) < dBlur)
+            curBlur = BLUR_TARGET_VELOCITY;
+        blurScript.velocityScale = curBlur;
+
 
         // If we should display the input then...
         if(isDisplayingInputPrompt){
@@ -60,7 +113,8 @@ public class YNTD_Controller : MonoBehaviour {
                 txtCredit.SetFadingIn(false);
 
                 isDisplayingInputPrompt = false;
-                
+                BLUR_TARGET_VELOCITY = 5;
+                BLUR_MIN_VELOCITY = 5;
 
                 // Enable the player to move around
                 FPSController.gameHasNotBegun = false;
@@ -92,8 +146,10 @@ public class YNTD_Controller : MonoBehaviour {
     void Trigger_One_Activated()
     {
         Debug.Log("YOU DRUNK IT!");
+        BLUR_TARGET_VELOCITY = 10;
+        BLUR_MIN_VELOCITY += 5;
         DrinkAlcohol();
-        FlareUpLightInWater();
+        StartCoroutine( FlareUpLightInWater());
     }
 
     // Makes the light in the water's intensity increase then die back down
@@ -103,29 +159,27 @@ public class YNTD_Controller : MonoBehaviour {
         if(isPoolLightingUp)
             StopAllCoroutines();
 
-        //TODO make these real hooks
-        float probeIntensity = 0;
-        float pointLightIntensity = 0;
-        
-
         // We are scaling two things to two different maximum intensities
         float LIGHT_PROBE_MAX_INTENSITY = 20;
-        float POINT_LIGHT_MAX_INTENSITY = 2;
+        float POINT_LIGHT_MAX_INTENSITY = 8;
         float dIntensity = .02f;
         
         // Turn lights on
-        while(probeIntensity < LIGHT_PROBE_MAX_INTENSITY || pointLightIntensity < POINT_LIGHT_MAX_INTENSITY)
+        while(poolProbe.intensity < LIGHT_PROBE_MAX_INTENSITY || poolLight.intensity < POINT_LIGHT_MAX_INTENSITY)
         {
-            probeIntensity += dIntensity*10;
-            pointLightIntensity += dIntensity;
+            poolProbe.intensity += dIntensity*10;
+            poolLight.intensity += dIntensity;
             yield return new WaitForSeconds(0.01f);
         }
 
+        // Let them stay on for a bit
+        yield return new WaitForSeconds(2);
+
         // Turn em off
-        while(probeIntensity > 0 || pointLightIntensity > 0)
+        while(poolProbe.intensity > 0 || poolLight.intensity > 0)
         {
-            probeIntensity -= dIntensity*10;
-            pointLightIntensity -= dIntensity;
+            poolProbe.intensity -= dIntensity*10;
+            poolLight.intensity -= dIntensity;
             yield return new WaitForSeconds(0.01f);
         }
 
@@ -134,7 +188,25 @@ public class YNTD_Controller : MonoBehaviour {
     // Blur camera a for a short moment to simulate drunkeness and make a sound of drinking
     void DrinkAlcohol()
     {
-        // TODO: Blur
+        Shake();
+        StartCoroutine( BlurForSeconds(5));
         AudioController.PlaySound(YNTD_AudioController.Sounds.SE_Gulp);
+    }
+
+    // Turns the blur on for n seconds
+    IEnumerator BlurForSeconds(int duration)
+    {
+        Debug.Log("BLURIIING!");
+        BLUR_TARGET_VELOCITY = BLUR_MAX_VELOCITY;
+        yield return new WaitForSeconds(duration);
+        BLUR_TARGET_VELOCITY = BLUR_MIN_VELOCITY;
+    }
+
+    void Shake()
+    {
+        originPosition = transform.position;
+        originRotation = transform.rotation;
+        shake_intensity = 500f;
+        shake_decay = 0.002f;
     }
 }
